0dc60b12f8af42c3612e9d0d66f6485683d173a3
Carlo Bilbao <cbilbao88@gmail.com>
2018-08-10 22:58:57 -0700

    make hero banner responsive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-
 import Home from './containers/home/home';
 
 import './App.css';
--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -13,13 +13,13 @@ const home = () => {
             <div className={classes.jumbo + " jumbotron jumbotron-fluid"}>
                 <div className={classes.jumboContent + " container"}>
                     <div className="row">
-                        <h1 className={classes.headline + " col-12 display-4 text-white text-center"}>Software Developer, Science Geek, Life-Long Learner</h1>
                         <p className={classes.name + " col-12 text-white text-center text-uppercase"}>Carlo Antonio Bilbao</p>
+                        <h1 className={classes.headline + " col-12 display-4 text-white text-center"}>Software Developer, Science Geek, Life-Long Learner</h1>
                         <ul className={classes.icons + " col-12 text-center"}>
-                            <li><a href="https://www.linkedin.com/in/carlo-bilbao/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-                            <li><a href="https://github.com/CarloAntonio" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
-                            <li><a href="https://www.instagram.com/carloantoniob/" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a></li>
-                            <li><a href="https://stackoverflow.com/users/8305585/carlo-antonio-bilbao" target="_blank"><FontAwesomeIcon icon={faStackOverflow} /></a></li>
+                            <li><a href="https://www.linkedin.com/in/carlo-bilbao/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                            <li><a href="https://github.com/CarloAntonio" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a></li>
+                            <li><a href="https://www.instagram.com/carloantoniob/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a></li>
+                            <li><a href="https://stackoverflow.com/users/8305585/carlo-antonio-bilbao" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faStackOverflow} /></a></li>
                         </ul>
                     </div>
                 </div>
